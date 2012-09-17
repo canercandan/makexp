7c5748f7e36c872d7c64bd5c124669d49b610879
Caner Candan <candan@info.univ-angers.fr>
2012-09-17 22:13:54 +0200

    + default_variables.py

---
+++ default_variables.py
@@ -0,0 +1,17 @@
+{
+    'DESCRIPTION': """\
+
+""",
+
+    'SAMPLES': [],
+    'NRUNS': 21,
+    'POPSIZES': [20],
+    'CORESIZES': [],
+    'BINARIES': [''],
+    'BINARYPATH': '',
+    'RESTART': False,
+    'DYNAMIC': False,
+    'EXECUTE': False,
+    'PLOT': True,
+    'PLOT_ON_WINDOW': True,
+}
